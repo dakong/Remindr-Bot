036dca15f2df2acdb20dec8ca8ebb1a1150f34cd
dakong <dakong94@gmail.com>
2016-10-13 20:52:22 -0700

    removed api calls

--- app/routes.js
+++ app/routes.js
@@ -5,11 +5,11 @@ var apiReminder = require('./APIWrapper.js');
 module.exports = function (app) {
 
   //Reminders API
-  app.post('/reminders', apiReminder.api.create);
-  app.get('/reminders/:recipientId', apiReminder.api.getAll);
-  app.get('/reminders/:recipientId/:reminderCount', apiReminder.api.getReminder);
-  app.put('/reminders/:reminder_id', apiReminder.api.update);
-  app.delete('/reminders/:reminder_id', apiReminder.api.delete);
+  //app.post('/reminders', apiReminder.api.create);
+  //app.get('/reminders/:recipientId', apiReminder.api.getAll);
+  //app.get('/reminders/:recipientId/:reminderCount', apiReminder.api.getReminder);
+  //app.put('/reminders/:reminder_id', apiReminder.api.update);
+  //app.delete('/reminders/:reminder_id', apiReminder.api.delete);
 
   //Reminder Bot api
   app.get('/api', ReminderBot.validateToken);
